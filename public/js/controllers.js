2a483d404b3d7f55da0f50f4cc90bed2e8b3cff1
Bo Sun <bsun@axioma.com>
2015-08-18 09:38:31 -0400

    fix issues

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -14,6 +14,7 @@ angular.module('myApp.controllers', [])
     $scope.cellPadding = 2;
     $scope.contentWidth = $scope.cellWidth - 2 * $scope.cellPadding;
     $scope.contentHeight = $scope.cellHeight - 2 * $scope.cellPadding;
+    $scope.showGridLines = true;
 
     $scope.droppedItems = [];
 
@@ -41,15 +42,7 @@ angular.module('myApp.controllers', [])
       $scope.test.push(a);
     }
 
-    var lines = [], lines2 = [];
-    for (var i = 1; i < $scope.colNum; i++) {
-      lines.push({x1: 0, y1: i * $scope.cellHeight, x2: $scope.tableWidth, y2: i * $scope.cellHeight});
-    }
-    $scope.lines = lines;
-    for (var i = 1; i < $scope.colNum; i++) {
-      lines2.push({y1: 0, x1: i * $scope.cellWidth, y2: $scope.tableWidth, x2: i * $scope.cellWidth});
-    }
-    $scope.lines2 = lines2;
+    gridLines($scope);
 
     // Drag and drop section
     $scope.dataid = 0;
@@ -57,7 +50,6 @@ angular.module('myApp.controllers', [])
     $scope.OnDragStart = function (evt) {
       var parentDraggable = evt.target.parentNode;
       while (parentDraggable) {
-        console.log(parentDraggable)
         if (parentDraggable.hasAttribute('jp-draggable')) {
           break;
         }
@@ -87,65 +79,100 @@ angular.module('myApp.controllers', [])
       }
       if (i < $scope.list.length) {
         var clone = angular.copy($scope.list[i], {});
-        clone.id = clone.id + '-dropped';
+        clone.id = clone.id + '-dropped' + new Date().getMilliseconds();
         $scope.droppedItems.push(clone);
       }
 
-
       $scope.$apply(function () {
         $timeout(function () {
+          var layoutInfo;
           for (var i = 0; i < $scope.droppedItems.length; i++) {
             console.log($scope.droppedItems[i])
             var elem = angular.element('#' + $scope.droppedItems[i]['id']);
             if (typeof elem.css('left') === 'undefined' || elem.css('left') === 'auto') {
-              var sizeInfo = getSize(elem, evt, $scope);
-              console.log(sizeInfo)
-              var snapX = sizeInfo.col * $scope.cellWidth;
+              layoutInfo = getLayoutInfo(elem, evt, $scope);
+              var snapX = layoutInfo.col * $scope.cellWidth;
               elem.css('left', snapX + 'px');
-              var snapY = sizeInfo.row * $scope.cellHeight;
+              var snapY = layoutInfo.row * $scope.cellHeight;
               elem.css('top', snapY + 'px');
-              console.log(snapX, snapY)
-              elem.css('width', sizeInfo.sizeX * $scope.contentWidth + 'px');
-              elem.css('height', sizeInfo.sizeY * $scope.contentHeight + 'px');
+              elem.css('width', layoutInfo.sizeX * $scope.contentWidth + 'px');
+              elem.css('height', layoutInfo.sizeY * $scope.contentHeight + 'px');
             } else if ($scope.dataid === $scope.droppedItems[i]['id']) {
-              var sizeInfo = getSize(elem, evt, $scope);
-              console.log(sizeInfo)
-              var snapX = sizeInfo.col * $scope.cellWidth;
+              layoutInfo = getLayoutInfo(elem, evt, $scope);
+              var snapX = layoutInfo.col * $scope.cellWidth;
               elem.css('left', snapX + 'px');
-              var snapY = sizeInfo.row * $scope.cellHeight;
+              var snapY = layoutInfo.row * $scope.cellHeight;
               elem.css('top', snapY + 'px');
-              console.log(snapX, snapY)
             }
           }
         });
       });
-      return;
     };
 
 
   });
 
-function getSize(elem, evt, $scope) {
+function getLayoutInfo(elem, evt, $scope) {
   // snap to grid
-  var col = 0, row = 0;
-  col = ~~( (evt.originalEvent.offsetX - $scope.rx ) / $scope.cellWidth);
-  // var snapX = whichCol * $scope.cellWidth;
-  // angular.element('#' + $scope.dataid).css('left', snapX + 'px');
-  row = ~~( (evt.originalEvent.offsetY - $scope.ry ) / $scope.cellHeight);
-  // var snapY = whichRow * $scope.cellHeight;
-  // angular.element('#' + $scope.dataid).css('top', snapY + 'px');
+  var col, row;
+  var offSetX = 0, offSetY = 0;
+  if ($scope.showGridLines) {
+    if (evt.target.nodeName === 'svg' || evt.target.id === 'container') {
+      console.log('in container')
+    } else {
+      console.log(evt);
+      console.log('not in container');
+      if (evt.target.hasAttribute('jp-draggable')) {
+        var dropTarget = evt.target;
+      } else {
+        // TODO find nearest parent which has attribute 'jp-draggable'
+        var dropTarget = evt.target.parentNode;
+      }
+
+      console.log(dropTarget);
+      console.log(angular.element(dropTarget).css('left'));
+      offSetX = parseInt(angular.element(dropTarget).css('left').slice(0, -2));
+      offSetY = parseInt(angular.element(dropTarget).css('top').slice(0, -2));
+    }
+  } else {
+    if (evt.target.id !== 'container') {
+      console.log('not in container')
+    } else {
+      console.log('in container')
+    }
+  }
+
+  console.log(evt.target.parentNode);
+
+  col = ~~( (evt.originalEvent.offsetX - $scope.rx + offSetX) / $scope.cellWidth);
+  row = ~~( (evt.originalEvent.offsetY - $scope.ry + offSetY) / $scope.cellHeight);
+
+  console.log(evt.originalEvent.offsetX, $scope.rx, offSetX)
+  console.log(evt.originalEvent.offsetY, $scope.ry, offSetY)
+  console.log(col, row)
 
   // figure out how many grids needed
   var width = elem.css('width') ? elem.css('width').slice(0, -2) : ($scope.contentWidth); // -2: remove px suffix
   var height = elem.css('height') ? elem.css('height').slice(0, -2) : ($scope.contentHeight);// -2: remove px suffix
   var sizeX = ~~(width / $scope.contentWidth) + 1;
   var sizeY = ~~(height / $scope.contentHeight) + 1;
-  /*angular.element('#' + $scope.dataid).css('width', actualWidth * $scope.contentWidth + 'px');
-   angular.element('#' + $scope.dataid).css('height', actualHeight * $scope.contentHeight + 'px');*/
+
   return {
     sizeX: sizeX,
     sizeY: sizeY,
     col: col,
     row: row
   }
+}
+
+function gridLines($scope) {
+  var lines = [], lines2 = [], i;
+  for (i = 1; i < $scope.colNum; i++) {
+    lines.push({x1: 0, y1: i * $scope.cellHeight, x2: $scope.tableWidth, y2: i * $scope.cellHeight});
+  }
+  $scope.lines = lines;
+  for (i = 1; i < $scope.colNum; i++) {
+    lines2.push({y1: 0, x1: i * $scope.cellWidth, y2: $scope.tableWidth, x2: i * $scope.cellWidth});
+  }
+  $scope.lines2 = lines2;
 }
